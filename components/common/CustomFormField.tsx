905ab111a215b3e03c987ef184b49ba071089a47
mohamed_sami <rosini259@gmail.com>
2024-10-16 18:29:39 +0300

    fix import in common file

--- components/common/CustomFormField.tsx
+++ components/common/CustomFormField.tsx
@@ -5,17 +5,17 @@ import ReactDatePicker from "react-datepicker";
 import { Control } from "react-hook-form";
 import PhoneInput from "react-phone-number-input";
 
-import { Checkbox } from "./ui/checkbox";
+import { Checkbox } from "../ui/checkbox";
 import {
   FormControl,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
-import { Textarea } from "./ui/textarea";
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Select, SelectContent, SelectTrigger, SelectValue } from "../ui/select";
+import { Textarea } from "../ui/textarea";
 
 export enum FormFieldType {
   INPUT = "input",
